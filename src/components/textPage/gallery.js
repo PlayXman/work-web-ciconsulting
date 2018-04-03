ff18d29a7bc24df57128b0bfcb39a20ee227f579
PlayXman <PlayXman@DESKTOP-Playman>
2018-04-04 01:58:30 +0200

    Block Gallery [done]

--- src/components/textPage/gallery.js
+++ src/components/textPage/gallery.js
@@ -1,6 +1,20 @@
 (function () {
 	var $galleryItems = $('.gallery__item');
 	if ($galleryItems.length) {
-		$('.gallery__item').swipebox();
+		$('.gallery__item').swipebox({
+			afterOpen: function(){
+				var $selectorClose = $("#swipebox-close");
+				var clickAction = "touchend click";
+
+				$selectorClose.unbind(clickAction);
+
+				$selectorClose.bind(clickAction, function(event){
+					event.preventDefault();
+					event.stopPropagation();
+
+					$.swipebox.close();
+				});
+			}
+		});
 	}
 })();
